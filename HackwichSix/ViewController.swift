4ebe77fa0712ec76b7c7268d31188a00021cf460
montiho8 <montiho8@hawaii.edu>
2022-10-19 22:30:35 -1000

    Completed Hackwich Six Steps 1-9 and problem set 1

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -7,8 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myFriendsArray = ["Sara", "Nicole","Grant"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myFriendsArray.count
 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        
+        let text = myFriendsArray[indexPath.row]
+        
+        cell?.textLabel?.text = text
+        
+        return cell!
+        
+        
+        
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
